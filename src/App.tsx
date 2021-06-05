f7968c63edb2c8d35d6d79a8246cc418db32af7a
admin@cryptomayor.io <admin>
2021-06-05 02:31:57 +0000

    adding code needed for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import Map from './components/Map'
+import mapboxgl from 'mapbox-gl'
+
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default
 
 function App() {
   return (
